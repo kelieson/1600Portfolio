13229bc0e114ad2e237b00a88d50f3246128349e
kelieson <91221480+kelieson@users.noreply.github.com>
2021-10-28 16:13:03 -0600

    first commit

---
+++ sw-chars/sw-chars.js
@@ -0,0 +1,61 @@
+import { people } from '../data/people.js'
+import { getLastNumber } from '../utility/index.js'
+
+const main = document.querySelector('#main')
+
+const mainHeader = document.createElement("header")
+document.body.insertBefore(mainHeader, main)
+
+const allButton = document.createElement('button')
+allButton.textContent = 'All Characters'
+allButton.addEventListener('click', () => populateDOM(people))
+mainHeader.appendChild(allButton)
+
+const maleButton = document.createElement("button")
+maleButton.textContent = 'Male Characters'
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+mainHeader.appendChild(femaleButton)
+
+const otherButton = document.createElement("button")
+otherButton.textContent = 'Other Characters'
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
+mainHeader.appendChild(otherButton)
+
+const maleCharacters = people.filter((person) => person.gender === 'male')
+
+const femaleCharacters = people.filter((person) => person.gender === 'female')
+
+const otherCharacters = people.filter((person) => {
+  if (
+    person.gender === 'n/a' ||
+    person.gender === 'hermaphrodite' ||
+    person.gender === 'none'
+  ) {
+    return person
+  }
+})
+
+function populateDOM(characters) {
+  while (main.firstChild) {
+    main.removeChild(main.firstChild)
+  }
+  
+Characters.forEach((element) => {
+  const personFig = document.createElement('figure')
+  const personImg = document.createElement('img')
+  let charNum = getLastNumber(element.url)
+  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+  const personCaption = document.createElement('figcaption')
+  personCaption.textContent = element.name
+
+  personFig.appendChild(personImg)
+  personFig.appendChild(personCaption)
+
+  main.appendChild(personFig)
+})
+}
---
+++ sw-films/starwars-films.js
@@ -0,0 +1,20 @@
+import { films } from '../data/films.js'
+import { getLastNumber } from '../utility/index.js'
+
+let filmList = document.querySelector('#filmList')
+
+for (let i = 0; i < films.length; i++) {
+  let figure = document.createElement('figure')
+  let figImg = document.createElement('img')
+  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+  let figCaption = document.createElement('figcaption')
+
+  const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
+
+  figCaption.textContent = foundFilm.title
+  
+  figure.appendChild(figImg)
+  figure.appendChild(figCaption)
+  
+  filmList.appendChild(figure)
+ }
