eeeea7d6193107b67f7c66038667384341b6f9b9
kelieson <91221480+kelieson@users.noreply.github.com>
2021-12-03 17:18:00 -0700

    started pokemon

---
+++ pokemon/pokecode.js
@@ -0,0 +1,12 @@
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`).
