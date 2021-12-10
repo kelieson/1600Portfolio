1c47d8c4384c1132bbcf22184650a4dc39b0c6dd
kelieson <91221480+kelieson@users.noreply.github.com>
2021-12-09 20:44:24 -0700

    added style, types, back divs, broke constructor

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -20,13 +20,9 @@ function loadPokemon(offset = 0, limit = 25) {
   });
 }
 
+loadPokemon(0, 25)
+
 const pokeGrid = document.querySelector(".pokeGrid");
-const loadButton = document.querySelector(".loadButton");
-loadButton.addEventListener("click", () => {
-    removeChildren(pokeGrid)
-    loadPokemon(100, 5)
-}
-);
 
 const moreButton = document.querySelector('.moreButton')
 moreButton.addEventListener('click', () => {
@@ -41,7 +37,8 @@ createButton.addEventListener("click", () => {
   let pokeHeight = prompt("How tall is this Pokemon?");
   let pokeWeight = prompt("How heavy is this Pokemon?");
   let pokeAbilities = prompt ("What are your Pokemon's abilities? (use a comma separated list)")
-  let newPokemon = new Pokemon(pokeName, pokeHeight, pokeWeight, getAbilitiesArray(pokeAbilities))
+  let pokeTypes = prompt ("Which type(s) is your Pokemon? (one or two, separated by a space)")
+  let newPokemon = new Pokemon(pokeName, pokeHeight, pokeWeight, getAbilitiesArray(pokeAbilities), getTypesArray(pokeTypes))
   populatePokeCard(newPokemon)
 });
 
@@ -54,6 +51,17 @@ function getAbilitiesArray(commaString) {
             }
         }
     })
+}
+
+function getTypesArray(spacedString) {
+  let tempArray = spacedString.split(' ')
+  return tempArray.map((typeName) => {
+    return {
+      type: {
+        name: typeName
+      },
+    }
+  })
 }
 
 class Pokemon {
@@ -87,7 +95,7 @@ function populateCardFront(pokemon) {
   pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
 
   const pokeCaption = document.createElement("figcaption");
-  pokeCaption.textContent = pokemon.name;
+  pokeCaption.textContent = `${pokemon.name.toUpperCase()}`;
   pokeFront.appendChild(pokeImg);
   pokeFront.appendChild(pokeCaption);
 
@@ -98,59 +106,113 @@ function populateCardFront(pokemon) {
 function typesBackground(pokemon, card) {
     let pokeType1 = pokemon.types[0].type.name
     let pokeType2 = pokemon.types[1]?.type.name
-    card.style.setProperty(
-        'background',
-        `linear-gradient(${getPokeTypeColor(pokeType1)}, ${getPokeTypeColor(pokeType2)})`)
-}
-
-function getPokeTypeColor(pokeType) {
-    let color
-    switch (pokeType) {
-        case 'grass':
-            color = '#00FF00'
-            break
-        case 'fire':
-            color = '#FF0000'
-            break
-        case 'water':
-            color = '#0000FF'
-            break
-        case 'bug':
-            color = '#BF7600'
-            break
-        case 'flying':
-            color = '#D1EDE6'
-            break
-        case 'poison':
-            color = '#BFF50D'
-            break
-        case 'electric':
-            color = '#FFEB01'
-            break
-        case 'psychic':
-            color = '#000000'
-            break
-        case 'normal':
-            color = '#D4E6B3'
-            break
-        case 'default':
-            color = '#EEEEEE'
-            break
+    if (!pokeType2) {
+      card.style.setProperty('background', getPokeTypeColor(pokeType1))
+    } else {
+      card.style.setProperty(
+          'background',
+          `linear-gradient(${getPokeTypeColor(pokeType1)}, ${getPokeTypeColor(pokeType2)})`)
     }
 }
 
 function populateCardBack(pokemon) {
   const pokeBack = document.createElement("div");
   pokeBack.className = "cardFace back";
+  
+  const abilDiv = document.createElement("div");
+  pokeBack.appendChild(abilDiv);
+  abilDiv.className = "abilDiv";
+
+  const typeDiv = document.createElement("div");
+  pokeBack.appendChild(typeDiv);
+  typeDiv.className = "typeDiv";
+
+  const typeLabel = document.createElement('h4')
+  typeLabel.textContent = "Types:";
+  typeDiv.appendChild(typeLabel);
+
   const label = document.createElement("h4");
   label.textContent = "Abilities:";
-  pokeBack.appendChild(label);
+  abilDiv.appendChild(label);
+
   const abilityList = document.createElement("ul");
   pokemon.abilities.forEach((abilityItem) => {
     let listItem = document.createElement("li");
-    listItem.textContent = abilityItem.ability.name;
+    listItem.textContent = `${abilityItem.ability.name[0].toUpperCase()}${abilityItem.ability.name.slice(1)}`;
     abilityList.appendChild(listItem);
   });
-  pokeBack.appendChild(abilityList);
+  const typesList = document.createElement('ul')
+  pokemon.types.forEach((pokeType) => {
+
+    let typeItem = document.createElement('li')
+    typeItem.textContent = pokeType.type.name
+    typesList.appendChild(typeItem)
+  })
+  typeDiv.appendChild(typesList)
+  abilDiv.appendChild(abilityList);
   return pokeBack;
+}
+
+function getPokeTypeColor(pokeType) {
+  let color
+  switch (pokeType) {
+      case 'grass':
+          color = '#67F70A'
+          break
+      case 'fire':
+          color = '#FC0C0B'
+          break
+      case 'water':
+          color = '#08517A'
+          break
+      case 'ground':
+          color = '#BFAC21'
+          break
+      case 'bug':
+          color = '#085764'
+          break
+      case 'flying':
+          color = '#085764'
+          break
+      case 'poison':
+          color = '#1D0728'
+          break
+      case 'electric':
+          color = '#969101'
+          break
+      case 'psychic':
+          color = '#8A0532'
+          break
+      case 'fighting':
+          color = '#800B11'
+          break
+      case 'normal':
+          color = '#ACA974'
+          break
+      case 'rock':
+          color = '#474026'
+          break
+      case 'ice':
+          color = '#103D43'
+          break
+      case 'ghost':
+          color = '#472B53'
+          break
+      case 'dragon':
+          color = '#29036A'
+          break
+      case 'dark':
+          color = '##2D221C'
+          break
+      case 'steel':
+          color = '#454545'
+          break
+      case 'fairy':
+          color = '#F87EA7'
+          break
+      case 'default':
+          color = '#EEEEEE'
+          break
+  }
+  return color
 }
